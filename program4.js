60dcac787c9e0c612249275c696e3d7968b399f2
Gauri Mahashabde <gauri2interact@gmail.com>
2022-12-01 15:22:56 +0530

    Problem statement 13 with solution

--- program4.js
+++ program4.js
@@ -16,3 +16,57 @@ var mostWordsFound = function (sentences) {
 
 let result = mostWordsFound(sentences);
 document.getElementById("element2").innerHTML = result;
+
+//problem statement 12 
+
+let arr = [-5, -4, -3, -2, 0, 2, 4, 6, 8];
+document.getElementById("element3").innerHTML = arr;
+
+function zeroSumPair(array) {
+    for (let numbers of array) {
+       console.log(`outer ${numbers}`);
+        for (let j = 1; j < array.length; j++){
+            console.log(`inner ${array[j]}`)
+            if(numbers + array[j] === 0){
+                return [numbers,array[j]];
+            }
+        }
+    }
+}
+
+const result1 = zeroSumPair([-5, -4, -3, -2, 0, 2, 4, 6, 8 ]);
+console.log(result1); 
+document.getElementById("element4").innerHTML = result1;
+
+//problem statement 13
+//solution type 1
+
+let address = "1.1.1.1";
+document.getElementById("element5").innerHTML = address;
+
+const defangIPaddr = (address) =>  address.replace(/\./g,"[.]");
+
+const output = defangIPaddr(address);
+document.getElementById("element6").innerHTML = output;
+
+//solution type 2
+//using for loop
+
+const defangIPaddr1 = function(address) {
+    let result =[];
+for (let i = 0 ; i<address.length ; i++){
+    if(address[i] === '.'){
+        result.push('[.]')
+    }
+    else{
+        result.push(address[i]);
+    }
+}
+  return result.join("")
+};
+const output2 = defangIPaddr(address);
+console.log(output2);
+
+
+
+
